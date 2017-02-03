95a52f441ca97473b3355a4309514ccaa6113201
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 16:11:40 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,49 +1,17 @@
-<?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
-?>
 <?php
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-$MyWebsite = 'http://www.mciindia.org/ViewDetails.aspx?ID=999';
-$html  = file_get_html($MyWebsite);
-//print $html;
-foreach($html->find("//*[@id='Name']") as $element)
-  {
-    if ($element)
-    {
-      $Name = $element->find(span,0);
-        echo $Name;
-    }
-    
-  }
-  
 
-?>
+
+$MAX_ID = 1;
+
+
+or($id = 1; $id <= $MAX_ID; $id++)
+{
+    $html = scraperwiki::scrape("http://www.mciindia.org/ViewDetails.aspx?ID=".$id);
+$dom = new simple_html_dom();
+   $dom->load($html);
+ echo $dom;
   
-  
-  
-  
+}
+  ?>

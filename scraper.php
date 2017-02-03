bd93a80587461561470b61dc06dcc85463629fc9
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 16:46:25 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,9 +1,9 @@
 <?php
 require 'scraperwiki.php';
-require 'scraperwiki/simple_html_dom.php';
+require 'scraperwiki/simple_html_html.php';
 
 
-/$MAX_ID = 3;
+$MAX_ID = 3;
 for($id = 1; $id <= $MAX_ID; $id++)
 {
    $MyWebsite = ("http://www.mciindia.org/ViewDetails.aspx?ID=".$id);
@@ -11,16 +11,16 @@ for($id = 1; $id <= $MAX_ID; $id++)
     
 
 
-   echo $info['doc_name'] = $dom->find('span[id=Name]')->plaintext;
-   echo $info['doc_fname'] = $dom->find('span[id="FatherName"]')->plaintext;
-   echo $info['doc_dob'] = $dom->find('span[id="DOB"]')->plaintext;
-   echo $info['doc_infoyear'] = $dom->find('span[id="lbl_Info"]')->plaintext;
-   echo $info['doc_regnum'] = $dom->find('span[id="Regis_no"]')->plaintext;
-   echo $info['doc_datereg'] = $dom->find('span[id="Date_Reg"]')->plaintext;
-   echo $info['doc_council'] = $dom->find('span[id="Lbl_Council"]')->plaintext;
-   echo $info['doc_qual'] = $dom->find('span[id="Qual"]')->plaintext;
-   echo $info['doc_qualyear'] = $dom->find('span[id="QualYear"]')->plaintext;
-   echo $info['doc_univ'] = $dom->find('span[id="Univ"]')->plaintext;
-   echo $info['doc_address'] = $dom->find('span[id="Address"]')->plaintext;
+   echo $info['doc_name'] = $html->find('span[id=Name]')->plaintext;
+   echo $info['doc_fname'] = $html->find('span[id="FatherName"]')->plaintext;
+   echo $info['doc_dob'] = $html->find('span[id="DOB"]')->plaintext;
+   echo $info['doc_infoyear'] = $html->find('span[id="lbl_Info"]')->plaintext;
+   echo $info['doc_regnum'] = $html->find('span[id="Regis_no"]')->plaintext;
+   echo $info['doc_datereg'] = $html->find('span[id="Date_Reg"]')->plaintext;
+   echo $info['doc_council'] = $html->find('span[id="Lbl_Council"]')->plaintext;
+   echo $info['doc_qual'] = $html->find('span[id="Qual"]')->plaintext;
+   echo $info['doc_qualyear'] = $html->find('span[id="QualYear"]')->plaintext;
+   echo $info['doc_univ'] = $html->find('span[id="Univ"]')->plaintext;
+   echo $info['doc_address'] = $html->find('span[id="Address"]')->plaintext;
 }
 ?>

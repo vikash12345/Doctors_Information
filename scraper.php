3e90e39bc556e2a778dc6df188aec68e361466ae
vikash12345 <vikash@backgroundcheck.email>
2017-02-03 16:51:17 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -10,11 +10,11 @@ require 'scraperwiki/simple_html_dom.php';
     
 
 
-echo  Name =     $html->find("//span[@id='Name']", 0)->plaintext; . '<br/>';
-echo FName =    $html->find("//span[@id='FatherName']", 0)->plaintext; . '<br/>';
-echo DOB =      $html->find("//span[@id='DOB']", 0)->plaintext; . '<br/>';
-echo lblinfo =  $html->find("//span[@id='lbl_Info']", 0)->plaintext;. '<br/>';
-echo reginfo =  $html->find("//span[@id='Regis_no']", 0)->plaintext; . '<br/>';
-echo Date-reg = $html->find("//span[@id='Date_Reg']", 0) ->plaintext;. '<br/>';  
+echo $Name =     $html->find("//span[@id='Name']", 0)->plaintext; . '<br/>';
+echo $FName =    $html->find("//span[@id='FatherName']", 0)->plaintext; . '<br/>';
+echo $DOB =      $html->find("//span[@id='DOB']", 0)->plaintext; . '<br/>';
+echo $lblinfo =  $html->find("//span[@id='lbl_Info']", 0)->plaintext;. '<br/>';
+echo $reginfo =  $html->find("//span[@id='Regis_no']", 0)->plaintext; . '<br/>';
+echo $Date-reg = $html->find("//span[@id='Date_Reg']", 0) ->plaintext;. '<br/>';  
 
 ?>

fb7b51ba3dd20e190e290034c620ad14e23fb677
vikash12345 <vikash@backgroundcheck.email>
2017-02-08 11:05:20 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -6,34 +6,37 @@
 	for ($pageLoop = 1; $pageLoop <= $MAX_ID; $pageLoop++) {
 		$html  = file_get_html($MyWebsite . $pageLoop);
 		if ($html) {
-            $Name		=	$html->find("//span[@id='Name']", 0);
-			$FName		=	$html->find("//span[@id='FatherName']", 0);
-			$DOB		=	$html->find("//span[@id='DOB']", 0);
-			$lblinfo	=	$html->find("//span[@id='lbl_Info']", 0);
-			$reginfo	=	$html->find("//span[@id='Regis_no']", 0);
-			$Datereg	=	$html->find("//span[@id='Date_Reg']", 0);
-            $state	=	$html->find("//span[@id='Lbl_Council']", 0);
-            $qua	=	$html->find("//span[@id='Qual']", 0);
-            $quayear	=	$html->find("//span[@id='QualYear']", 0);
-            $University	=	$html->find("//span[@id='Univ']", 0);
-            $address	=	$html->find("//span[@id='Address']", 0);
+             $info['$Name']			=	$html->find("//span[@id='Name']", 0);
+	     $info['$FName']			=	$html->find("//span[@id='FatherName']", 0);
+	    $info['$DOB']			=	$html->find("//span[@id='DOB']", 0);
+	    $info['$lblinfo']			=	$html->find("//span[@id='lbl_Info']", 0);
+	    $info['$reginfo']			=	$html->find("//span[@id='Regis_no']", 0);
+	    $info['$Datereg']			=	$html->find("//span[@id='Date_Reg']", 0);
+            $info['$state']			=	$html->find("//span[@id='Lbl_Council']", 0);
+            $info['$qua']			=	$html->find("//span[@id='Qual']", 0);
+            $info['$quayear']			=	$html->find("//span[@id='QualYear']", 0);
+            $info['$University']		=	$html->find("//span[@id='Univ']", 0);
+            $info['$address']			=	$html->find("//span[@id='Address']", 0);
 
 
 
-            
-			
-			if ($Name) {
-		echo	'Name # : ' . $Name->plaintext . '<br/>';
-  		echo	'Father Name # : ' . $FName->plaintext . '<br/>';
-                echo    '$DOB # :' .$DOB->plaintext.'<br/>';
-                echo    '$Lable-Info # :' .$lblinfo->plaintext.'<br/>';
-                echo    '$Region-Information # :' .$reginfo->plaintext.'<br/>';
-                echo    '$Date_Of Reg # :' .$Datereg->plaintext.'<br/>';
-                echo    '$State # :' .$state->plaintext.'<br/>';
-                echo    '$Qualification # :' .$qua->plaintext.'<br/>';
-                echo    '$Qua_Year # :' .$quayear->plaintext.'<br/>';
-                echo    '$University Name # :' .$University->plaintext.'<br/>';
-                echo    '$Address Name # :' .$address->plaintext.'<br/>';
+             scraperwiki::save_sqlite(array('mci_snum','registration_number'), 
+    	array('mci_snum' => $id, 
+          'name' => (trim($info['doc_name'])), 
+          'fathers_name' => (trim($info['doc_fname'])),
+          'date_of_birth' => (trim($info['doc_dob'])),
+          'information_year' => (trim($info['doc_infoyear'])),
+          'registration_number' => (trim($info['doc_regnum'])),
+          'date_of_reg' => (trim($info['doc_datereg'])),
+          'council' => (trim($info['doc_council'])),
+          'qualifications' => (trim($info['doc_qual'])),
+          'qualification_year' => (trim($info['doc_qualyear'])),
+          'permanent_address' => (trim($info['doc_address']))
+    ));
+    
+  //clean out the dom
+  $dom->__destruct();
+							
 
 
                 
